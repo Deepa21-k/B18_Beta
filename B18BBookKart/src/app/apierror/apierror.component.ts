12b7e56dcc2e1c09d6ef7bc9678121ae75d25c52
Deepa21-k <deepamkjc29@gmail.com>
2022-03-05 14:04:55 +0530

    Created component

---
+++ B18BBookKart/src/app/apierror/apierror.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apierror',
+  templateUrl: './apierror.component.html',
+  styleUrls: ['./apierror.component.scss']
+})
+export class ApierrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- B18BBookKart/src/app/app.module.ts
+++ B18BBookKart/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApierrorComponent } from './apierror/apierror.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApierrorComponent
   ],
   imports: [
     BrowserModule,
